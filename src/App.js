afd0e43636c5aceb02f936eedd9f86c73f9099b5
ANAGU DANIEL <anagudaniel@gmail.com>
2022-11-25 03:54:50 -0800

    'new'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <Router>
+      <Routes>
+        <Route path='/' element={}/>
+      </Routes>
+     </Router>
     </div>
   );
 }
 
 export default App;
+ 
