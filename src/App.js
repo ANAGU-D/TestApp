0587c49a04738b1a3be43f1dc173642d75a00afd
ANAGU DANIEL <anagudaniel@gmail.com>
2022-11-25 08:19:10 -0800

    neww

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Body from './components/body/body';
+import Header from './components/header/header';
+import Landingpage from './components/landingpage/landingpage';
 
 function App() {
   return (
     <div className="App">
      <Router>
+      <Header/>
       <Routes>
-        <Route path='/' element={<Body/>}/>
+        <Route path='/' element={<Landingpage/>}/>
+        <Route path='/body' element={<Body/>}/>
       </Routes>
      </Router>
     </div>
